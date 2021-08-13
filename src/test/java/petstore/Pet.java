98f9cc3a777b661d6429b7075d871a72cf469d34
elisapsan <elisangela.espinoza@hotmail.com>
2021-08-12 23:07:43 -0300

    Minha primeira automação com IntelliJ e REST Assured.

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,108 @@
+package petstore;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.contains;
+
+public class Pet {
+
+    String uri = "https://petstore.swagger.io/v2/pet";
+
+    public String lerJson(String caminhoJson) throws IOException {
+
+      return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+    @Test(priority = 1)
+
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
+
+            given()
+                    .contentType("application/json")
+                    .log().all()
+                    .body(jsonBody)
+            .when()
+                    .post(uri)
+            .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("name", is("Boris"))
+                    .body("status", is("available"))
+                    .body("category.name",is("ELISANGELA2509"))
+                    .body("tags.name", contains("data"))
+            ;
+
+    }
+    @Test(priority = 2)
+
+    public void consultarPet() {
+
+        String petId = "2509197725";
+
+        String token =
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                        .when()
+                        .get(uri + "/" + petId)
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name", is("Boris"))
+                        .body("category.name", is("ELISANGELA2509"))
+                        .body("status", is("available"))
+                        .extract()
+                        .path("category.name");
+        System.out.println("O token é: " + token);
+
+
+    }
+    @Test(priority = 3)
+
+    public void alterarPet() throws IOException {
+    String jsonBody = lerJson("db/pet2.json");
+
+    given()
+
+            .contentType("application/json")
+            .log().all()
+            .body(jsonBody)
+    .when()
+            .put(uri)
+    .then()
+            .log().all()
+            .statusCode(200)
+            .body("name", is("Boris") )
+            .body("status", is("sold"))
+     ;
+    }
+
+    @Test(priority = 4)
+
+    public void excluirPet(){
+
+        String petId = "2509197725";
+
+    given()
+            .contentType("application/json")
+            .log().all()
+    .when()
+            .delete(uri + "/" + petId)
+    .then()
+            .log().all()
+            .statusCode(200)
+            .body("code", is(200))
+            .body("type", is("unknown"))
+            .body("message", is(petId))
+    ;
+
+
+
+    }
+}
